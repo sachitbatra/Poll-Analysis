aeb5cde22178427ea5c331e80376eb823767c242
Sachit Batra <sachitbatra97@gmail.com>
2019-02-06 12:17:13 +0530

    Fixed District results - Group by statement

--- resultsDistrict.php
+++ resultsDistrict.php
@@ -14,7 +14,7 @@
   $connector = mysql_connect($host,$username,$password);
   $selected = mysql_select_db("elecanalysis", $connector);
   $candidate_votes=mysql_query("SELECT b.c_id, b.name 'candidate', c.party_id, c.name 'party', d.constituency_id, d.name 'constituency', e.district_id, e.name 'district', f.state_id, f.name 'state', COUNT(a.c_id) 'votes' FROM vote_bank a, candidate b, political_party c, constituency d, district e, state f WHERE a.c_id = b.c_id AND b.political_party_id = c.party_id AND b.constituency_id = d.constituency_id AND d.district_id = e.district_id AND e.state_id = f.state_id GROUP BY a.c_id");
-  $result = mysql_query("SELECT party_id, party, district_id, district, SUM(votes) 'noVotes' FROM candidate_votes GROUP BY party_id, state_id");
+  $result = mysql_query("SELECT party_id, party, district_id, district, SUM(votes) 'noVotes' FROM candidate_votes GROUP BY party_id, district_id");
   ?>
 
 <div id="resultDiv">
